37a025f45f3bfee1dddabc61cddff7bde2eb7446
weixuefeng <weixuefeng@lubangame.com>
2021-08-11 10:36:25 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          hello world
+        </h1>
+        <p>
+          lets' get started
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
